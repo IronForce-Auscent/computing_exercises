86555a5adb46c60a915c5c7879ab53fb056b40ab
Solitude Nox <64062496+IronForce-Auscent@users.noreply.github.com>
2023-03-21 01:17:51 +0000

    .

--- s3_exercises/exec93.py
+++ s3_exercises/exec93.py
@@ -6,11 +6,17 @@ use your solution to Exercise 92 while completing this exercise.
 """
 
 def exercise_93(n):
-  i = 2
+  i = n + 1
+  while True:
+    if is_prime(n):
+      return i
+    i += 1
+    
+def is_prime(n):
+  i =2
   while i <= (n/2):
     if n % i != 0:
       i += 1
     else:
       return False
   return True
-    
