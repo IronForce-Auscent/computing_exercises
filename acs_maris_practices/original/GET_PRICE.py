0a087b025f47fa2fb641f46d2c1a63f7d7337b89
Solitude Nox <64062496+IronForce-Auscent@users.noreply.github.com>
2023-06-30 02:17:15 +0000

    .

--- acs_maris_practices/original/GET_PRICE.py
+++
@@ -1,5 +0,0 @@
-def get_price(option):
-    cake_list = ["A", "B", "C", "D","E","F","G","H"]
-    price_list = [25, 22, 38, 35, 15, 40, 53, 20]
-    position = cake_list.index(option)
-    return price_list[position]
